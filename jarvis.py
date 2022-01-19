dd0e7009648955bf8f801ac7743609edf13909fd
firelord69 <79193827+firelord69@users.noreply.github.com>
2022-01-19 10:44:26 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,171 @@
+import subprocess
+from time import strftime
+import wolframalpha
+import pyttsx3
+import tkinter
+import json
+import random
+import operator
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import winshell
+import pytz
+import pyjokes
+import feedparser
+import smtplib
+import ctypes
+import datetime
+import requests
+import shutil
+from twilio.rest import Client
+from bs4 import BeautifulSoup
+import win32com.client as wincl
+from urllib.request import urlopen
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+	engine.say(audio)
+	engine.runAndWait()
+
+def wishMe():
+	hour = int(datetime.datetime.now().hour)
+	if hour>= 0 and hour<12:
+		speak("Good Morning Sir !")
+
+	elif hour>= 12 and hour<18:
+		speak("Good Afternoon Sir !")
+
+	else:
+		speak("Good Evening Sir !")
+
+	assname =("Jarvis 1 point o")
+	speak("I am your Assistant")
+	speak(assname)
+	
+
+def username():
+	speak("Welcome Mister Pranshul")
+	columns = shutil.get_terminal_size().columns
+	
+	print("#####################".center(columns))
+	print("Welcome Mr. Pranshul")
+	print("#####################".center(columns))
+	
+	speak("How can i Help you, Sir")
+
+def takeCommand():
+	
+	r = sr.Recognizer()
+	
+	with sr.Microphone() as source:
+		
+		print("Listening...")
+		r.pause_threshold = 1
+		audio = r.listen(source)
+
+	try:
+		print("Recognizing...")
+		query = r.recognize_google(audio, language ='en-in')
+		print(f"User said: {query}\n")
+
+	except Exception as e:
+		print(e)
+		print("Unable to Recognize your voice.")
+		return "None"
+	
+	return query
+
+
+
+if __name__ == '__main__':
+    clear = lambda: os.system('cls')
+     
+    clear()
+    wishMe()
+    username()
+     
+    while True:
+         
+        query = takeCommand().lower()
+         
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences = 3)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+ 
+        elif 'open youtube' in query:
+            speak("Here you go to Youtube\n")
+            webbrowser.open("youtube.com")
+ 
+        elif 'open google' in query:
+            speak("Here you go to Google\n")
+            webbrowser.open("google.com")
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")   
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'time in london' in query:
+            tz_London = pytz.timezone('Europe/London')
+            datetime_London = datetime.datetime.now(tz_London)
+            strTime = datetime_London.strftime("%H:%M:%S")
+            speak(f"Sir, the time in london is {strTime}")
+            print(f"Sir, the time in london is {strTime}")
+
+        elif 'thanks' in query:
+            speak(f"It's My Pleasure Sir.")
+
+        elif "write a note" in query:
+            speak("What should i write, sir")
+            note = takeCommand()
+            file = open('jarvis.txt', 'w')
+            speak("Sir, Should i include date and time")
+            snfm = takeCommand()
+            if 'yes' in snfm or 'sure' in snfm:
+                strTime = datetime.datetime.now().strftime("% H:% M:% S")
+                file.write(strTime)
+                file.write(" :- ")
+                file.write(note)
+            else:
+                file.write(note)
+
+        elif "where is" in query:
+            query = query.replace("where is", "")
+            location = query
+            speak("User asked to Locate")
+            speak(location)
+            webbrowser.open("https://www.google.nl/maps/place/" + location + "")
+
+        elif 'shutdown system' in query:
+                speak("Hold On a Sec ! Your system is on its way to shut down")
+                subprocess.call('shutdown/p/f')
+
+        elif 'empty recycle bin' in query:
+            winshell.recycle_bin().empty(confirm = False, show_progress = False, sound = True)
+            speak("Recycle Bin Recycled")
+
+        elif "who are you" in query:
+            speak("I am your virtual assistant created by Pranshul Upadhyay for his project")
+
+        elif 'search' in query or 'play' in query:
+             
+            query = query.replace("search", "")
+            query = query.replace("play", "")         
+            webbrowser.open(query)
+
+        elif 'how are you' in query:
+            speak("I am fine, Thank you")
+            speak("How are you, Sir")
+ 
+        elif 'fine' in query or "good" in query:
+            speak("It's good to know that your fine")
